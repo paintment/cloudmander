10574baa7b523e1c5954a4a1610c97b3af07b4f0
stantric <153416066+stantric@users.noreply.github.com>
2023-12-10 00:48:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,20 @@ cloudmander_ascii = """
   ╚█████╔╝███████╗╚█████╔╝╚██████╔╝██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║██████╔╝███████╗██║░░██║
   ░╚════╝░╚══════╝░╚════╝░░╚═════╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝
                                       d e v e l o p e d  b y  s t a n t r i c  o n  g i t h u b
+                                                       ----------------------------------------
+"""
+
+command_legend = """
+    [gui], [connect -ip -auth], [savelog], [log], [openlog],
+    [closegui], [disconnect], [serverinfo], [saveserver], [saved],
+    [deletesaves], [saveanddisconnect], [redbutton], [reset]
+    ---
+    For help on how to use a command, use [help --command {command}]
+    Use Ctrl + G to open the GUI faster
+    Thanks for using cloudmander!
 """
 
 # intro
 os.system("cls")
-print(fade.greenblue(cloudmander_ascii))
+print(fade.purplepink(cloudmander_ascii))
+print(fade.purplepink(command_legend))
